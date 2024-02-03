a7b1a086d84e094577beeaf7fc4b6e8a1aeb4e02
amandabrousson <amanda.brousson@gmail.com>
2024-02-03 10:42:39 -0300

    clase 02

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,60 +1,34 @@
-console.log(tempData);
+const addFilm = () => {
+$.get(`https://students-api.2.us-1.fl0.io/movies/`, (data, status) => {
+    // ownRepo.createMovie(data);
+    // console.log(ownRepo.getmovies());
+    console.log(data);
+    aHTML(data)
+});
+}
 
-const miRepo= tempData.map((pelicula) => {return aHTML(pelicula);}); 
+const container = document.getElementById("cuerpo")
 
-// Funcion para extraer propiedades
-function aHTML(pelicula){
-    const { title, year, director, duration, genre, rate, poster } = pelicula;
+//a esta funcion le entra la data obtenida de la API
+function aHTML(data){
 
-// Creo los elementos HTML
-const mititulo = document.createElement("h3");
-const miurl = document.createElement("img");
-const miaño = document.createElement("p");
-const midirector = document.createElement("p");
-const miduracion = document.createElement("p");
-const migenero = document.createElement("p");
-const mirate = document.createElement("p");
-
-// Le asigno sus propiedades 
-/* mititulo.id = id; */
-mititulo.textContent = title;
-miurl.src = poster;
-miaño.textContent = `Año: ${year}`;
-midirector.textContent =`Director: ${director}`;
-miduracion.textContent = `Duración: ${duration}`;
-migenero.textContent = `Género: ${genre}`;
-mirate.textContent = `Calificación: ${rate}`;;
-
-const activityCard = document.createElement("div");
-
-// agrego los elementos al nuevo div
-activityCard.appendChild(mititulo);
-activityCard.appendChild(miurl);
-activityCard.appendChild(miaño);
-activityCard.appendChild(midirector);
-activityCard.appendChild(miduracion);
-activityCard.appendChild(migenero);
-activityCard.appendChild(mirate);
-
-activityCard.classList.add('estilocartas');
-
-// agrego al div grande
-document.getElementById("cuerpo").appendChild(activityCard);
-
-// agrego clases css
-mititulo.classList.add("cont-titulo");
-miurl.classList.add("cont-url");
-miaño.classList.add("cont-año");
-midirector.classList.add("cont-director");
-miduracion.classList.add("cont-duracion");
-migenero.classList.add("cont-genero");
-mirate.classList.add("cont-rate");
-
-// retorno el div card
-
-return activityCard;
+    //recorres el arreglo que viene de la API
+    data.forEach(element => {
+        
+        const activityCard = document.createElement("div"); // creas el div de las cards
+        activityCard.classList.add("estilocartas");
+        //mandas todo al DOM
+        activityCard.innerHTML =` 
+            <h2>${element.title}</h2>
+            <img src=${element.poster}</img>
+            <p>${element.year}</p>
+        `
+        //mando todo al contenedor grande
+        container.append(activityCard) 
+    });
 };
-
+//llamo a la funcion
+addFilm()
 
   
 
