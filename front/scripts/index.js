db8cfee1a6ab5f9385cdc6da02da905874398f38
amandabrousson <amanda.brousson@gmail.com>
2024-02-28 15:21:09 -0300

    28 02 avances front

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,5 +1,5 @@
 const tempData = require('./tempData'); // Importo 'tempData' con datos de películas de respaldo.
-const renderCards = require('./rendercards'); // Importo el módulo 'rendercards', que se encarga de renderizar las tarjetas de películas.
+const { renderCards, openCard, closeCard } = require('./rendercards');
 const axios = require("axios"); // Importo la biblioteca 'axios' para realizar solicitudes HTTP.
 
 /* const addFilm = () => { 
@@ -9,15 +9,13 @@ $.get(`https://students-api.2.us-1.fl0.io/movies/`, (data, status) => {
 */
 
 const fetchMovies = async () =>{ //defino funcion asíncrona
-    /* try{ */ // Intento hacer una solicitud GET a la api.
         const data = await axios.get("http://localhost:3000/movies"); // los datos se guardan en data.
         renderCards(data.data); // Llamo a la función 'renderCards' con los datos obtenidos de la API.
-    /* } catch(err){
-        renderCards(tempData); // En caso de error al hacer la solicitud, llamo a 'renderCards'.
-    } */
+    
 };
 
 fetchMovies();
+
 
 
 
--- front/scripts/rendercards.js
+++ front/scripts/rendercards.js
@@ -1,22 +1,47 @@
 // rendercards.js
-
-
 const container = document.getElementById("cuerpo");
 
 function renderCards(data) {
-
     data.forEach(element => {
         const activityCard = document.createElement("div");
         activityCard.classList.add("estilocartas");
         activityCard.innerHTML = `
-            <img src=${element.poster}></img>
-            <h2><a href= "#">${element.title}</a></h2>
+            <img class= "cartita" src=${element.poster}></img>
+            <h2 class="titulopeli" >${element.title}</h2>
             <p>${element.year}</p>
         `;
         container.appendChild(activityCard);
 
+        const cartita = activityCard.querySelector('.cartita');
+        cartita.addEventListener("click", () => openCard(element, activityCard));
     });
-    
 }
 
-module.exports = renderCards;
+function openCard(data, activityCard) {
+    const genresString = data.genre.join(', ');
+    const cuerpocartita = document.createElement("div");
+    cuerpocartita.classList.add("estilocartita");
+    cuerpocartita.innerHTML = `
+    <li><p>Director: ${data.director}</p></li>
+    <li><p>Género: ${genresString}</p></li>
+    <li><p>Duración: ${data.duration}</p></li>
+    <li><p>Calificación: ${data.rate}</p></li>
+    <div class="video-container">${data.trailer}</div>
+    <button class="cerrarBtn" onclick="closeCard()">Cerrar</button>
+    `;
+    activityCard.appendChild(cuerpocartita);
+    const cerrarBtn = cuerpocartita.querySelector('.cerrarBtn');
+    cerrarBtn.addEventListener("click", () => closeCard(cuerpocartita));
+}
+
+function closeCard(cardElement) {
+    if (cardElement.parentElement) {
+        cardElement.parentElement.removeChild(cardElement);
+    }
+}
+
+module.exports = {
+    renderCards,
+    openCard,
+    closeCard
+};
