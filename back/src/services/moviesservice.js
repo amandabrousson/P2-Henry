7146fa0f3251a661762d2a8568d41b224ae9323c
amandabrousson <amanda.brousson@gmail.com>
2024-03-06 16:21:11 -0300

    06 marzo

--- back/src/services/moviesservice.js
+++ back/src/services/moviesservice.js
@@ -7,51 +7,6 @@ const apiMovie= {
 
 const Movie = require("../modelos/movie");
 
-let nextId = 1;
-
-/* class Movies {
-    constructor(title, year, director, duration, genre, rate, poster, trailer) {
-        if (!title || !director || !year) {
-            throw new Error("Propiedades no válidas");
-        }
-        this.id = nextId++;
-        this.title = title;
-        this.year = year;
-        this.director = director;
-        this.duration = duration;
-        this.genre = genre;
-        this.rate = parseFloat(rate);
-        this.poster = poster;
-        this.trailer = trailer;
-    }
-} */
-
-/* const movielist = [];
-
-const GuardiansVol2 = new Movies("Guardians of the Galaxy Vol. 2", 2017, "James Gunn", "2h 16min", ["Action", "Adventure", "Comedy"], 7.7, "https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg", '<iframe width="560" height="315" src="https://www.youtube.com/embed/wUn05hdkhjM?si=kDTurcy2nQfNlQrG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
-movielist.push(GuardiansVol2);
-
-const starwarsIV = new Movies("Star Wars: Episode IV - A New Hope", 1977, "George Lucas", "2h 1min", ["Action", "Adventure", "Fantasy", "Sci-Fi"], 8.7, "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg", '<iframe width="560" height="315" src="https://www.youtube.com/embed/vZ734NWnAHA?si=FnLG2Kl9TanbUEjq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
-movielist.push(starwarsIV);
-
-const lordofrings1 = new Movies("The Lord of the Rings: The Fellowship of the Ring", 2001, "Peter Jackson", "2h 58min", ["Action", "Adventure", "Drama", "Fantasy"], 8.8, "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg", '<iframe width="560" height="315" src="https://www.youtube.com/embed/3GJp6p_mgPo?si=pNoCZdwgy5UkTr45" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
-movielist.push(lordofrings1);
-
-const titanic = new Movies("Titanic", 1988, "James Cameron", "3h 30m", ["Drama", "Adventure", "Romantic"], 7.9, "https://pics.filmaffinity.com/titanic-321994924-large.jpg", 'Harry');
-movielist.push(titanic);
-
-const harryPotter1 = new Movies("Harry Potter and the philosopher`s stone", 2001, "Chris Columbus", "2h 32min", ["Fantasy", "Adventure"], 7.6, "https://musicart.xboxlive.com/7/92e05000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080", '<iframe width="560" height="315" src="https://www.youtube.com/embed/uwM2oPGQMl4?si=bvotmroWSZyOk51u" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
-movielist.push(harryPotter1);
-
-const DavinciCode = new Movies("The Da Vinci Code", 2006, "Ron Howard", "2h 29min", ["Mysthery", "Thriller"], 6.6, "https://m.media-amazon.com/images/M/MV5BZTY3OTE2ZTktMDlmNC00NDY2LTg1NWUtNjcxNTNiYzhjYjMxXkEyXkFqcGdeQXVyMTI2NTQ4MDUw._V1_FMjpg_UX1000_.jpg", '<iframe width="560" height="315" src="https://www.youtube.com/embed/5sU9MT8829k?si=N9MQJXfD4tQXWHeh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
-movielist.push(DavinciCode);
-
-const efectoMariposa = new Movies("Butterfly Effect", 2004, "Eric Bress J.", "1h 53min", ["Suspense", "Sci-Fi"], 7.3, "https://m.media-amazon.com/images/M/MV5BODNiZmY2MWUtMjFhMy00ZmM2LTg2MjYtNWY1OTY5NGU2MjdjL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", '<iframe width="560" height="315" src="https://www.youtube.com/embed/kkWQLjI2rAU?si=kVPDRl3fB4LhdySM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
-movielist.push(efectoMariposa);
-
-const interestelar = new Movies("Intrestellar", 2014, "Christopher Nolan", "2h 49min", ["Adventure", "Sci-Fi"], 8.7, "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg", '<iframe width="560" height="315" src="https://www.youtube.com/embed/zSWdZVtXT7E?si=7PZGslc99RKLDddu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
-movielist.push(interestelar); */
-
 module.exports = {
 
     getAllMovies: async () => {
@@ -80,8 +35,8 @@ module.exports = {
     },
     
     getMovieByDuration: async(duration) =>{
-        const moviesWithDuration = await Movie.find({ duration: { $gt: duration } });
-            return moviesWithDuration;
+        const movieByDuration = await Movie.findOne({ duration });
+        return movieByDuration;
     },
 
     getMovieByGenre: async (genre) => {
--- front/scripts/formulario.js
+++ front/scripts/formulario.js
@@ -8,12 +8,13 @@ const enviar = async () => {
     const year = document.getElementById('year').value;
     const director = document.getElementById('director').value;
     const duration = document.getElementById('duration').value;
-    const genre = document.getElementById('genre').value.split(',');
+    const genreCheckboxes = document.querySelectorAll('input[name="nombrec"]:checked');
+    const genre = Array.from(genreCheckboxes).map(checkbox => checkbox.value);
     const rate = document.getElementById('rate').value;
     const poster = document.getElementById('poster').value;
     const trailer = document.getElementById('trailer').value;
 
-    if (!titulo || !year || !director || !duration || !genre || !rate || !poster || !trailer) {
+    if (!titulo || !year || !director || !duration || genre.length === 0 || !rate || !poster || !trailer) {
         alert("Por favor, completa todos los campos.");
         return;
     }
@@ -21,17 +22,19 @@ const enviar = async () => {
     try {
         const response = await axios.post("http://localhost:3000/movies", {
             title: titulo,
-            year: parseInt(year), // Convertir año a número
+            year: parseInt(year), 
             director,
             duration,
             genre,
-            rate: parseFloat(rate), // Convertir tasa a número decimal
+            rate: parseFloat(rate), 
             poster,
             trailer,
         });
 
-        console.log(response.data); // Puedes mostrar la respuesta del servidor en la consola
-        refreshFormulario(); // Limpiar el formulario después de enviar la película
+        console.log(response.data); 
+        refreshFormulario(); 
+
+        alert("La película se agregó correctamente.");
     } catch (error) {
         console.error("Error al enviar datos:", error);
     }
